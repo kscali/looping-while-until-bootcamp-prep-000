484ceff8eb13c4bab65713e8974e7d8bf0b25ea8
kosatun@aol.com <kosatun@aol.com>
2018-07-11 22:19:50 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,7 +1,11 @@
 def using_until
   levitation_force = 6
   
-  until levitation_force == 10  
+  until levitation_force == 10 do
+    puts "Wingardium Leviosa"
+    leviation_force += 1
+    
+  end  
     
 end
 
